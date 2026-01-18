39e571e40ee3a4776153e180c0ad721383f48621
IGMcreate <isaacmirza2@gmail.com>
2026-01-18 21:45:11 +0000

    Changed missed files

--- commands/common/removeRole.js
+++ commands/common/removeRole.js
@@ -1,35 +1,6 @@
-// const { ApplicationCommandOptionType, PermissionsBitField} = require('discord.js');
+const { ApplicationCommandOptionType, PermissionsBitField} = require('discord.js');
 
-// module.exports = {
-//     name: 'unrole',
-//     description: "Removes role from target",
-//     options: [
-//         {
-//             name: 'target',
-//             description: 'person to unrole',
-//             type: ApplicationCommandOptionType.User,
-//             required: true,
-//         },
-//         {
-//             name: 'role',
-//             description: 'role to remove',
-//             type: ApplicationCommandOptionType.Role,
-//             required: true,
-//         }
-//     ],
-//     async execute({inter}) {
-//         if(inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.ManageRoles)||inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)){
-//         const role = inter.options.getRole('role');
-//         const member = inter.options.getMember('target');
-//         member.roles.remove(role);
-//         inter.editReply(`Success!`)
-//         } else
-//         await inter.editReply(`You don't have the required permissions`)
-//     },
-// };
-import { ApplicationCommandOptionType, PermissionsBitField } from 'discord.js';
-
-export default {
+module.exports = {
     name: 'unrole',
     description: "Removes role from target",
     options: [
@@ -46,15 +17,13 @@ export default {
             required: true,
         }
     ],
-    async execute({ inter }) {
-        if (inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.ManageRoles) ||
-            inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)) {
-            const role = inter.options.getRole('role');
-            const member = inter.options.getMember('target');
-            member.roles.remove(role);
-            inter.editReply(`Success!`);
-        } else {
-            await inter.editReply(`You don't have the required permissions`);
-        }
+    async execute({inter}) {
+        if(inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.ManageRoles)||inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)){
+        const role = inter.options.getRole('role');
+        const member = inter.options.getMember('target');
+        member.roles.remove(role);
+        inter.editReply(`Success!`)
+        } else
+        await inter.editReply(`You don't have the required permissions`)
     },
 };
--- commands/common/setAvatar.js
+++ commands/common/setAvatar.js
@@ -1,46 +1,6 @@
-// const { ApplicationCommandOptionType, PermissionsBitField, EmbedBuilder} = require('discord.js');
+const { ApplicationCommandOptionType, PermissionsBitField, EmbedBuilder} = require('discord.js');
 
-// module.exports = {
-//     name: 'setavatar',
-//     description: "Sets the avatar to a URL or path",
-//     options: [
-//         {
-//             name: 'image',
-//             description: 'placeholder',
-//             type: ApplicationCommandOptionType.Attachment,
-//             required: true,
-//         }
-//     ],
-//     async execute({ client, inter }) {
-//         const image = inter.options.getAttachment('image');
-//         const avatar = image.url;
-
-
-//         if(inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)){
-//             const changed = await client.user.setAvatar(avatar).catch(err =>{
-//                 inter.editReply({content:`Error: ${err}` , ephemeral:true});
-//             });
-
-
-//             if(changed) {
-//                 const embed = new EmbedBuilder()
-//                 .setColor("Blue")
-//                 .setDescription(`Avatar is set to: \`${avatar}\``)
-    
-//                 await inter.editReply({embeds: [embed], ephemeral: true});    
-//             } else {
-//                 return;
-//             }
-
-//         } else {
-//             await inter.editReply(`You don't have the required permissions`)
-//         }
-
-//     },
-// };
-import { ApplicationCommandOptionType, PermissionsBitField, EmbedBuilder } from 'discord.js';
-
-export default {
+module.exports = {
     name: 'setavatar',
     description: "Sets the avatar to a URL or path",
     options: [
@@ -53,25 +13,28 @@ export default {
     ],
     async execute({ client, inter }) {
         const image = inter.options.getAttachment('image');
-        if (!image) return inter.editReply({ content: 'No image provided', ephemeral: true });
-
         const avatar = image.url;
 
-        if (!inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)) {
-            return inter.editReply({ content: "You don't have the required permissions", ephemeral: true });
-        }
 
-        try {
-            const changed = await client.user.setAvatar(avatar);
-            if (changed) {
+        if(inter.member.permissionsIn(inter.channel).has(PermissionsBitField.Flags.Administrator)){
+            const changed = await client.user.setAvatar(avatar).catch(err =>{
+                inter.editReply({content:`Error: ${err}` , ephemeral:true});
+            });
+
+
+            if(changed) {
                 const embed = new EmbedBuilder()
-                    .setColor('Blue')
-                    .setDescription(`Avatar is set to: \`${avatar}\``);
-
-                await inter.editReply({ embeds: [embed], ephemeral: true });
+                .setColor("Blue")
+                .setDescription(`Avatar is set to: \`${avatar}\``)
+    
+                await inter.editReply({embeds: [embed], ephemeral: true});    
+            } else {
+                return;
             }
-        } catch (err) {
-            await inter.editReply({ content: `Error: ${err.message || err}`, ephemeral: true });
+
+        } else {
+            await inter.editReply(`You don't have the required permissions`)
         }
+
     },
 };
